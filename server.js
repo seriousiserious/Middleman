cf8284a296409bab31e520465a84cdd17a2b6d5c
seriousiserious <135375767+seriousiserious@users.noreply.github.com>
2023-06-14 22:58:04 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60,
+    },
+    http: {
+        port: 8000,
+        mediaroot: './media',
+        webroot: './www',
+        allow_origin: '*',
+        api: true,
+    },
+    trans: {
+        ffmpeg: 'C:\\FFmpeg\\bin\\ffmpeg.exe',
+        tasks: [
+            {
+                app: 'live',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+            },
+        ],
+    },
+};
+
+var nms = new NodeMediaServer(config);
+nms.run();
